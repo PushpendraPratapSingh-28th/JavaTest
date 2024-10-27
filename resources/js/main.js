f8732ddb61e8088a5a692d9dc470a425922689b8
PushpendraPratapSingh <165888464+PushpendraPratapSingh-28th@users.noreply.github.com>
2024-10-27 19:13:59 +0530

    Add files via upload

---
+++ resources/js/main.js
@@ -0,0 +1,371 @@
+// AOS Script
+AOS.init();
+
+function checkAnswer1() {
+    var answer = document.getElementById("answer1").value;
+    if (answer.toLowerCase() === "301.5656") {
+      document.getElementById("result1").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result1").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer2() {
+    var answer = document.getElementById("answer2").value;
+    if (answer.toLowerCase() === "-128 to 127") {
+      document.getElementById("result2").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result2").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer3() {
+    var answer = document.getElementById("answer3").value;
+    if (answer.toLowerCase() === "-3.4e+038") {
+      document.getElementById("result3").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result3").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer4() {
+    var answer = document.getElementById("answer4").value;
+    if (answer.toLowerCase() === "all of the mentioned") {
+    document.getElementById("result4").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result4").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer5() {
+    var answer = document.getElementById("answer5").value;
+    if (answer.toLowerCase() === "-32768 to 32767") {
+    document.getElementById("result5").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result5").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer6() {
+    var answer = document.getElementById("answer6").value;
+    if (answer.toLowerCase() === "32") {
+      document.getElementById("result6").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result6").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer7() {
+    var answer = document.getElementById("answer7").value;
+    if (answer.toLowerCase() === "all statements are correct") {
+      document.getElementById("result7").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result7").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer8() {
+    var answer = document.getElementById("answer8").value;
+    if (answer.toLowerCase() === "false") {
+      document.getElementById("result8").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result8").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer9() {
+    var answer = document.getElementById("answer9").value;
+    if (answer.toLowerCase() === "true") {
+        document.getElementById("result9").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result9").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer10() {
+    var answer = document.getElementById("answer10").value;
+    if (answer.toLowerCase() === "true") {
+      document.getElementById("result10").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result10").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer11() {
+    var answer = document.getElementById("answer11").value;
+    if (answer.toLowerCase() === "66") {
+      document.getElementById("result11").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result11").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer12() {
+    var answer = document.getElementById("answer12").value;
+    if (answer.toLowerCase() === "boolean b3 = false;") {
+      document.getElementById("result12").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result12").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer13() {
+    var answer = document.getElementById("answer13").value;
+    if (answer.toLowerCase() === "0 to 65535") {
+      document.getElementById("result13").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result13").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer14() {
+    var answer = document.getElementById("answer14").value;
+    if (answer.toLowerCase() === "unicode") {
+      document.getElementById("result14").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result14").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer15() {
+    var answer = document.getElementById("answer15").value;
+    if (answer.toLowerCase() === "ASCII and ISO-LATIN1") {
+      document.getElementById("result15").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result15").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer16() {
+    var answer = document.getElementById("answer16").value;
+    if (answer.toLowerCase() === "65 97") {
+      document.getElementById("result16").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result16").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer17() {
+    var answer = document.getElementById("answer17").value;
+    if (answer.toLowerCase() === "3 4 4") {
+      document.getElementById("result17").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result17").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer18() {
+    var answer = document.getElementById("answer18").value;
+    if (answer.toLowerCase() === "1") {
+      document.getElementById("result18").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result18").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer19() {
+    var answer = document.getElementById("answer19").value;
+    if (answer.toLowerCase() === "1.5 1") {
+      document.getElementById("result19").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result19").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer20() {
+    var answer = document.getElementById("answer20").value;
+    if (answer.toLowerCase() === "1, 2, 3 & 4") {
+      document.getElementById("result20").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result20").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer21() {
+    var answer = document.getElementById("answer21").value;
+    if (answer.toLowerCase() === "None of the mentioned") {
+      document.getElementById("result21").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result21").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer22() {
+    var answer = document.getElementById("answer22").value;
+    if (answer.toLowerCase() === "Both Numeric & Characters") {
+      document.getElementById("result22").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result22").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer23() {
+    var answer = document.getElementById("answer23").value;
+    if (answer.toLowerCase() === "Both Integers and floating - point numbers") {
+      document.getElementById("result23").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result23").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer24() {
+    var answer = document.getElementById("answer24").value;
+    if (answer.toLowerCase() === "5.640000000000001 5") {
+      document.getElementById("result24").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result24").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer25() {
+    var answer = document.getElementById("answer25").value;
+    if (answer.toLowerCase() === "32") {
+      document.getElementById("result25").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result25").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer26() {
+    var answer = document.getElementById("answer26").value;
+    if (answer.toLowerCase() === "True") {
+      document.getElementById("result26").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result26").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer27() {
+    var answer = document.getElementById("answer27").value;
+    if (answer.toLowerCase() === "256 0") {
+      document.getElementById("result27").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result27").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer28() {
+    var answer = document.getElementById("answer28").value;
+    if (answer.toLowerCase() === "7 2") {
+      document.getElementById("result28").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result28").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer29() {
+    var answer = document.getElementById("answer29").value;
+    if (answer.toLowerCase() === "~") {
+      document.getElementById("result29").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result29").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer30() {
+    var answer = document.getElementById("answer30").value;
+    if (answer.toLowerCase() === "3 1 6") {
+      document.getElementById("result30").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result30").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer31() {
+    var answer = document.getElementById("answer31").value;
+    if (answer.toLowerCase() === "The right shift operator automatically fills the higher order bits with 0") {
+      document.getElementById("result31").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result31").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer32() {
+    var answer = document.getElementById("answer32").value;
+    if (answer.toLowerCase() === "5") {
+      document.getElementById("result32").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result32").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer33() {
+    var answer = document.getElementById("answer33").value;
+    if (answer.toLowerCase() === "42 -43") {
+      document.getElementById("result33").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result33").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer34() {
+    var answer = document.getElementById("answer34").value;
+    if (answer.toLowerCase() === "<=") {
+      document.getElementById("result34").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result34").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer35() {
+    var answer = document.getElementById("answer35").value;
+    if (answer.toLowerCase() === ">>") {
+      document.getElementById("result35").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result35").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer36() {
+    var answer = document.getElementById("answer36").value;
+    if (answer.toLowerCase() === "31") {
+      document.getElementById("result36").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result36").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer37() {
+    var answer = document.getElementById("answer37").value;
+    if (answer.toLowerCase() === "false") {
+      document.getElementById("result37").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result37").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer38() {
+    var answer = document.getElementById("answer38").value;
+    if (answer.toLowerCase() === "false true") {
+      document.getElementById("result38").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result38").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer39() {
+    var answer = document.getElementById("answer39").value;
+    if (answer.toLowerCase() === "2") {
+      document.getElementById("result39").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result39").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer40() {
+    var answer = document.getElementById("answer40").value;
+    if (answer.toLowerCase() === "Boolean") {
+      document.getElementById("result40").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result40").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer41() {
+    var answer = document.getElementById("answer41").value;
+    if (answer.toLowerCase() === "&&") {
+      document.getElementById("result41").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result41").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer42() {
+    var answer = document.getElementById("answer42").value;
+    if (answer.toLowerCase() === "false") {
+      document.getElementById("result42").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result42").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer43() {
+    var answer = document.getElementById("answer43").value;
+    if (answer.toLowerCase() === "3") {
+      document.getElementById("result43").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result43").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer44() {
+    var answer = document.getElementById("answer44").value;
+    if (answer.toLowerCase() === "1, 2 & 3") {
+      document.getElementById("result44").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result44").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer45() {
+    var answer = document.getElementById("answer45").value;
+    if (answer.toLowerCase() === "true and false are non numeric values") {
+      document.getElementById("result45").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result45").innerHTML = "Incorrect. Try again.";
+    }
+  }
+  function checkAnswer46() {
+    var answer = document.getElementById("answer46").value;
+    if (answer.toLowerCase() === "Boolean") {
+      document.getElementById("result46").innerHTML = "Correct!";
+    } else {
+      document.getElementById("result46").innerHTML = "Incorrect. Try again.";
+    }
+  }
